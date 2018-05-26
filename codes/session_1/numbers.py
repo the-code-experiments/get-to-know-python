cea3982377114bc5ba7ed86956dd99ff0792bbfa
ashegde <ashegde@paypal.com>
2018-05-26 21:25:04 +0530

    feat(example): numbers

---
+++ codes/session_1/numbers.py
@@ -0,0 +1,38 @@
+# Open terminal > python3
+# Start typing below commands and see the output
+
+2 + 2
+45 - 5
+
+# Division always returns a floating point number
+45 / 5
+
+# To avoid floating in division
+45 // 5
+
+17 % 2
+
+5 * 2
+
+5 ** 2 # 5 square, 5 to the power of 2
+
+width = 45
+height = 45
+width * height
+
+# List printed expression is assign to `_`
+a = 100
+b = 200
+c = 250
+
+a + b # 300
+c + _ # 550 (250 + 300)
+
+
+
+
+
+
+
+
+
