bfc9ec0dd6d105fd0c72bda5047730e58af348b6
ashegde <ashegde@paypal.com>
2018-05-27 04:34:47 +0530

    feat(examples): loops

---
+++ codes/session_2/loop.py
@@ -0,0 +1,35 @@
+# Open terminal > python3 loop.py
+# Start typing below commands and see the output
+
+animals = ['cat', 'dog', 'racoon']
+for a in animals:
+  print(a, len(a))
+
+print('\n')
+
+for i in range(5):
+  print(i)
+
+print('\n')
+
+for i in range(5, 10):
+  print(i)
+
+print('\n')
+
+# (range start, range end, step) 
+for i in range(0, 10, 3):
+  print(i)
+
+print('\n')
+
+for i in range(-10, -100, -30):
+  print(i)
+
+print('\n')
+
+a = ['My', 'name', 'is', 'Ashwin']
+for i in range(len(a)):
+  print(i, a[i])
+
+print('\n')
