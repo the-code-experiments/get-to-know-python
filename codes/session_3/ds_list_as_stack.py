4d32f7ae23beb0c99b0a53cb1b0af9202cf0bd7d
ashegde <ashegde@paypal.com>
2018-05-27 14:58:18 +0530

    feat(examples): data structure list

---
+++ codes/session_3/ds_list_as_stack.py
@@ -0,0 +1,19 @@
+# Open terminal > python3 ds_list_as_stack.py (Data Structure)
+# Start typing below commands and see the output
+
+# Stack:
+# Last-in First-out
+
+birds = ['Owl', 'Macaw', 'Parrot', 'BeeHumming'] 
+
+# Append to stack i.e. Last-In
+print('Append to stack i.e. Last-In: ')
+birds.append('Panguin')
+birds.append('Kingfisher')
+print(birds)
+
+# Pop from the stack i.e. First-out
+print('Pop from the stack i.e. First-out: ')
+birds.pop()
+birds.pop()
+print(birds)
