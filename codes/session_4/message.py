665eede5ca1e8c5ddc6560030861260e22f6d270
ashegde <ashegde@paypal.com>
2018-06-16 16:18:09 +0530

    feat(example): modules

---
+++ codes/session_4/message.py
@@ -0,0 +1,9 @@
+# message.py is a module (imported inside module.py)
+# so execute python3 module.py
+# Start typing below commands and see the output
+
+def hello(name):
+  print('Hello ' + name)
+
+def welcome(name):
+  print('Welcome ' + name)
